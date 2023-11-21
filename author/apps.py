b1e87b891712ebe16bda6d8d05116bbaf8d4a297
zh <1812643720@qq.com>
2023-11-21 21:46:07 +0800

    update app

--- author/apps.py
+++ author/apps.py
@@ -7,4 +7,4 @@ class ScholarConfig(AppConfig):
     name = "author"
 
     def ready(self):
-        connections.create_connection(hosts=[ELAS_HOST])
+        connections.create_connection(hosts=['http://100.99.200.37:9200'])
--- work/apps.py
+++ work/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
 from elasticsearch_dsl import connections
+from config import ELAS_HOST
 
 class PaperConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "work"
 
     def ready(self):
-        connections.create_connection(hosts=['localhost'])
+        connections.create_connection(hosts=['http://100.99.200.37:9200'])
