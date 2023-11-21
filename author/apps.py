e9e20293337272d1914e7da4b5e5c060a0108a1d
zh <1812643720@qq.com>
2023-11-21 20:04:19 +0800

    delete document

--- author/apps.py
+++ author/apps.py
@@ -6,4 +6,4 @@ class ScholarConfig(AppConfig):
     name = "author"
 
     def ready(self):
-        connections.create_connection(hosts=['localhost'])
+        connections.create_connection(hosts=['http://100.99.200.37:9200'])
