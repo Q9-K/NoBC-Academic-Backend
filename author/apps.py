205a51d40b594b16efa6252d30e85c245765a950
Q9-K <20374319@buaa.edu.cn>
2023-11-21 21:01:27 +0800

    添加elasticsearch示例

--- author/apps.py
+++ author/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 from elasticsearch_dsl import connections
-
+from config import ELAS_HOST
 class ScholarConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "author"
 
     def ready(self):
-        connections.create_connection(hosts=['http://100.99.200.37:9200'])
+        connections.create_connection(hosts=[ELAS_HOST])
