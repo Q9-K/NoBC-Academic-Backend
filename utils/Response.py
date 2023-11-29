3f447cec2efd6b927a2fc728447be033ceb2404d
zh <1812643720@qq.com>
2023-11-29 12:58:15 +0800

    fix utils.Response param

--- utils/Response.py
+++ utils/Response.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from NoBC.commons import Commons
 
 
-def response(msg, code=Commons.SUCCESS, data=None):
+def response(code=Commons.SUCCESS, msg='', data=None):
     return JsonResponse({
         'code': code,
         'msg': msg,
