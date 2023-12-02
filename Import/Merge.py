ff926f1f864ccfe049a5f42d1bc9b0cf6196b964
Q9-K <20374319@buaa.edu.cn>
2023-12-03 04:04:29 +0800

    merge文档

---
+++ Import/Merge.py
@@ -0,0 +1,63 @@
+import os
+import json
+import time
+import gzip
+import csv
+from tqdm import tqdm
+from datetime import datetime
+from elasticsearch_dsl import connections
+from elasticsearch.helpers import parallel_bulk
+from itertools import islice
+
+def run(client, file_name, index_name):
+    with gzip.open(file_name, 'rt', encoding='utf-8') as file:
+        i = 0
+        data_list = []
+        print("start handling file {}".format(file_name))
+        start_time = time.perf_counter()
+        csv_reader = csv.reader(file)
+        csv_reader = islice(csv_reader, 1, None)
+        for row in csv_reader:
+            i += 1
+            from_id = "https://openalex.org/"+row[1]
+            data_list.append(
+                {
+                    "_op_type": "delete",
+                    "_index": index_name,
+                    "_id": from_id
+                }
+            )
+            if i % 5000 == 0:
+                start_time1 = time.time()
+                for ok, response in parallel_bulk(client=client, actions=data_list, chunk_size=5000, ignore_status=404):
+                    if ok:
+                        print(response)
+                data_list = []
+                end_time1 = time.time()
+                print("circle {} process time = {}s".format(int(i / 5000), end_time1 - start_time1))
+                exit(0)
+        if data_list:
+            start_time1 = time.time()
+            for ok, response in parallel_bulk(client=client, actions=data_list, chunk_size=5000, ignore_status=404):
+                if ok:
+                    print(response)
+            data_list = []
+            end_time1 = time.time()
+            print("circle {} process time = {}s".format(int(i / 5000), end_time1 - start_time1))
+        end_time = time.perf_counter()
+        print("finished handling file {} process time= {} min, end at {}"
+              .format(file_name, (end_time - start_time) / 60, datetime.now()))
+
+if __name__ == "__main__":
+    cl = connections.create_connection(hosts=['localhost'])
+    print("Start merge data at {}".format(datetime.now()))
+    root_path = '/data/openalex-snapshot/data/merged_ids'
+    sub_folders = [f for f in os.listdir(root_path) if os.path.isdir(os.path.join(root_path, f))]
+    for sub_folder in tqdm(sub_folders):
+        folder_path = os.path.join(root_path, sub_folder)
+        files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+        for zip_file in files:
+            file_name = os.path.join(folder_path, zip_file)
+            # print(sub_folder)
+            run(cl, file_name, index_name=sub_folder[:-1])
+    print("Finished merge data at{}".format(datetime.now()))
