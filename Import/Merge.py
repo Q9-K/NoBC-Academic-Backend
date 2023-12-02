b06dfb29cd4f2467ef1125ba16c71fbb8533b36a
Q9-K <20374319@buaa.edu.cn>
2023-12-03 05:11:18 +0800

    修复Merge Bug

--- Import/Merge.py
+++ Import/Merge.py
@@ -15,40 +15,28 @@ def run(client, file_name, index_name):
         print("start handling file {}".format(file_name))
         start_time = time.perf_counter()
         csv_reader = pd.read_csv(file_name, compression='gzip')
-        print(csv_reader.shape[0])
+        print('Need to merge count: ', csv_reader.shape[0])
         for index, row in csv_reader.iterrows():
             i += 1
             from_id = "https://openalex.org/"+row['id']
-            data_list.append(from_id)
+            data_list.append({
+                "_op_type": "delete",
+                "_index": index_name,
+                "_id": from_id
+            })
             if i % 5000 == 0:
                 start_time1 = time.time()
-                query = {
-                    "query": {
-                        "terms": {
-                            "_id": data_list
-                        }
-                    }
-                }
-                search = Search(using=client, index=index_name).update_from_dict(query)
-                response = search.delete()
-                if response.deleted > 0:
-                    print(f"Successfully deleted {response.deleted} documents.")
+                for ok, response in parallel_bulk(client=client, actions=data_list, chunk_size=5000, ignore_status=404):
+                    if ok:
+                        print(response)
                 data_list = []
                 end_time1 = time.time()
                 print("circle {} process time = {}s".format(int(i / 5000), end_time1 - start_time1))
         if data_list:
             start_time1 = time.time()
-            query = {
-                "query": {
-                    "terms": {
-                        "_id": data_list
-                    }
-                }
-            }
-            search = Search(using=client, index=index_name).update_from_dict(query)
-            response = search.delete()
-            if response.deleted > 0:
-                print(f"Successfully deleted {response.deleted} documents.")
+            for ok, response in parallel_bulk(client=client, actions=data_list, chunk_size=5000, ignore_status=404):
+                if ok:
+                    print(response)
             data_list = []
             end_time1 = time.time()
             print("circle {} process time = {}s".format(int(i / 5000), end_time1 - start_time1))
@@ -59,7 +47,7 @@ def run(client, file_name, index_name):
 if __name__ == "__main__":
     cl = connections.create_connection(hosts=['localhost'])
     print("Start merge data at {}".format(datetime.now()))
-    root_path = '/data/openalex-snapshot/data/merged_ids'
+    root_path = 'J:/openalex-snapshot/data/merged_ids'
     sub_folders = [f for f in os.listdir(root_path) if os.path.isdir(os.path.join(root_path, f))]
     for sub_folder in tqdm(sub_folders):
         folder_path = os.path.join(root_path, sub_folder)
