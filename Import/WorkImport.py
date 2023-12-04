b42105d142ad05b89fb4b1f5842eaf6266486b4c
Q9-K <20374319@buaa.edu.cn>
2023-12-05 00:47:40 +0800

    测试优化写入

--- Import/WorkImport.py
+++ Import/WorkImport.py
@@ -110,7 +110,7 @@ def generate_actions(file_name):
 
 def run(file_name):
     actions = generate_actions(file_name)
-    for success, info in parallel_bulk(client=cl, actions=actions, thread_count=8, chunk_size=1000, queue_size=20):
+    for success, info in parallel_bulk(client=cl, actions=actions, thread_count=8, chunk_size=2000, queue_size=30):
         if not success:
             print(f'Failed to index document: {info}')
 
