71d26bf5284e20f7b009938dab9765d434633ed9
Q9-K <20374319@buaa.edu.cn>
2023-12-05 19:40:47 +0800

    测试优化写入

--- Import/WorkImport.py
+++ Import/WorkImport.py
@@ -116,7 +116,7 @@ def run(file_name):
 
 def process_files(folder_path):
     files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-    with ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor(max_workers=8) as executor:
         futures = [executor.submit(run, os.path.join(folder_path, file)) for file in files]
         for future in futures:
             future.result()
