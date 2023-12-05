ca8c369986b02bd539c6247df5c7a93f462dbab8
shyJyt <2873704761@qq.com>
2023-12-05 21:58:57 +0800

    feat:author view

--- author/urls.py
+++ author/urls.py
@@ -1,6 +1,10 @@
 from .views import *
 from django.urls import path
 urlpatterns = [
-    path('common_search/', common_search),
+    path('get_author_by_name/', get_author_by_name),
+    path('get_author_by_id/', get_author_by_id),
     path('get_works/', get_works),
+    path('get_hot_authors/', get_hot_authors),
+    path('get_related_authors/', get_related_authors),
+    path('get_scholar_metrics/', get_scholar_metrics),
 ]
