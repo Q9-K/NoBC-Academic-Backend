1894340366551b3103b3d428c934d359782c0db5
zh <1812643720@qq.com>
2023-12-05 20:42:13 +0800

    add view_func in instition

--- utils/Response.py
+++ utils/Response.py
@@ -2,9 +2,10 @@ from django.http import JsonResponse
 from NoBC.commons import Commons
 
 
-def response(code=Commons.SUCCESS, msg='', data=None):
+def response(code=Commons.SUCCESS, msg='', data=None, error=False):
     return JsonResponse({
         'code': code,
         'msg': msg,
-        'data': data
+        'data': data,
+        'error': error
     })
