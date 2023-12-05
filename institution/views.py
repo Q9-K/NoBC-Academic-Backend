4a589ca9485c377e49f8933999426fa0e51f841f
zh <1812643720@qq.com>
2023-12-05 23:57:54 +0800

    add pagination func

--- institution/views.py
+++ institution/views.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from elasticsearch_dsl import Search, connections, Q
 
 from NoBC.status_code import *
@@ -11,6 +13,31 @@ ES_NAME = 'institution'
 ES_CONN = connections.create_connection(alias=ES_NAME, hosts=[ELAS_HOST], timeout=20)
 
 
+def pagination(search, request):
+    """
+    分页查询,将基础查询语句进行分页处理,返回具有分页功能的查询语句
+    :param request: 请求对象
+    :param search: Search对象
+    :return: 查询语句
+    """
+    size = int(request.GET.get('size', 10))
+    last_sort = request.GET.getlist('last_sort', None)
+    page = int(request.GET.get('page', 1))
+    _from = (page - 1) * size
+    # 不需要after,使用(from, size)查询
+    if last_sort is None:
+        _from = (page - 1) * size
+        ret = search[_from: size]
+    # 使用search_after查询
+    else:
+        after_body = {
+            'search_after': last_sort,
+            'size': size
+        }
+        ret = search.update_from_dict(after_body)
+    return ret
+
+
 def getInstitutionList(request):
     """
     获取机构列表
@@ -21,25 +48,20 @@ def getInstitutionList(request):
     if request.method == 'GET':
         # 获取参数,使用search_after分页,需要获取:
         # 每页大小size,上一页最后一个数据的sort字段(若为"",默认为第一页)
-        size = int(request.GET.get('size', 10))
-        last_sort = request.GET.getlist('last_sort', None)
-        data = []
+        institutions = []
         # 默认id升序
         search = Search(using=ES_CONN, index='institution').query(Q('match_all')).sort('id')
         # 不需要after,使用(from, size)查询
-        if last_sort is None:
-            ret = search[0: size]
-        # 使用search_after查询
-        else:
-            after_body = {
-                'search_after': last_sort,
-                'size': size
-            }
-            ret = search.update_from_dict(after_body)
-        for ele in ret.execute().to_dict()['hits']['hits']:
+        pagination_search = pagination(search, request)
+        for ele in pagination_search.execute().to_dict()['hits']['hits']:
             dic = ele['_source']
             dic['sort'] = ele['sort']
-            data.append(dic)
+            institutions.append(dic)
+        search: Search
+        total = search.count()
+        data = dict()
+        data['total'] = total
+        data['institutions'] = institutions
         return response(SUCCESS, '查询成功', data)
     else:
         return response(METHOD_ERROR, '请求方式错误', error=True)
@@ -78,25 +100,19 @@ def getInstitutionByKeyword(request):
         # 获取参数,使用search_after分页,需要获取:
         # 每页大小size,上一页最后一个数据的sort字段(若为"",默认为第一页)
         keyword = request.GET.get('keyword', "")
-        size = int(request.GET.get('size', 10))
-        last_sort = request.GET.getlist('last_sort', None)
-        data = []
+        institutions = []
         # 默认id升序
         search = Search(using=ES_CONN, index='institution').query('match', display_name=keyword).sort('id')
-        # 不需要after,使用(from, size)查询
-        if last_sort is None:
-            ret = search[0: size]
-        # 使用search_after查询
-        else:
-            after_body = {
-                'search_after': last_sort,
-                'size': size
-            }
-            ret = search.update_from_dict(after_body)
-        for ele in ret.execute().to_dict()['hits']['hits']:
+        pagination_search = pagination(search, request)
+        for ele in pagination_search.execute().to_dict()['hits']['hits']:
             dic = ele['_source']
             dic['sort'] = ele['sort']
-            data.append(dic)
-        return response(SUCCESS, '查询成功', data)
+            institutions.append(dic)
+        search: Search
+        total = search.count()
+        data = dict()
+        data['total'] = total
+        data['institutions'] = institutions
+        return response(SUCCESS, '查询成功', institutions)
     else:
         return response(METHOD_ERROR, '请求方式错误', error=True)
