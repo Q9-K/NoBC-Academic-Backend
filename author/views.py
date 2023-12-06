550f66496b5ca1f35a7fa280b28492283110996d
forgivingsoldier <2283666472@qq.com>
2023-12-06 12:03:03 +0800

    Merge branch 'dev' of https://github.com/Q9-K/NoBC-Academic into dev

--- author/views.py
+++ author/views.py
@@ -1,81 +1,72 @@
 from django.http import JsonResponse
 from elasticsearch_dsl import Search
 from elasticsearch_dsl.connections import connections
-
-# def search_view(request):
-#     #从json中获取数据
-#     data = json.loads(request.body)
-#     text = data['text']
-#     page = data['page']
-#
-#     # #从url中获取数据
-#     # text = request.GET.get('text')
-#     # page = request.GET.get('page')
-#
-#     base_cache_key = hashlib.md5(f'search_results_{text}'.encode()).hexdigest()
-#     cache_key = f'{base_cache_key}_page_{page}'
-#
-#     cached_results = cache.get(cache_key)
-#     if cached_results is not None:
-#         return JsonResponse({'code': 200, 'data': cached_results})
-#
-#     from .documents import ScholarDocument
-#     search = ScholarDocument.search().sort('_doc')
-#     results = search.filter("match", name=text).to_queryset()
-#     results = list(results.values())
-#     sort_and_cache_results.delay(results, base_cache_key)
-#     task = sort_and_cache_results.delay(results, cache_key)
-#     # task_result = AsyncResult(task.id)
-#     first_page = task_result.get(timeout=10)
-#
-#     return JsonResponse({
-#         'code': 200,
-#         'data': first_page,
-#     })
-
-
-# import time
-# from elasticsearch_dsl import connections, Document, Integer, Keyword, Text, Nested, Search
-# from elasticsearch.helpers import bulk, parallel_bulk
-# cl = connections.create_connection()
-# def search_view2(request):
-#     start_time = time.time()
-#     text = request.GET.get('text')
-#     s = Search(using=cl, index='author').filter('match', name=text)
-#     response = s.execute()
-#     results = []
-#     for hit in response:
-#         results.append(hit.to_dict())
-#     end_time = time.time()
-#     return JsonResponse({
-#         'count': len(results),
-#         'results': results,
-#         'time': end_time - start_time
-#     })
+from author.models import Author
+from NoBC.status_code import *
 
 elasticsearch_connection = connections.get_connection()
 
 
-# 作者名搜索
-def common_search(request):
-    author_name = request.GET.get('author_name')
-    search = Search(using=elasticsearch_connection, index='author').filter('match', display_name=author_name)
-    search_results = search.execute()
-    results = []
-    for hit in search_results:
-        results.append(hit.to_dict())
+# 根据作者名搜索，需要分页，只取前1w条以内的数据
+def get_author_by_name(request):
+    if request.method == 'GET':
+        author_name = request.GET.get('author_name')
+        page_num = int(request.GET.get('page_num', 1))
+        page_size = int(request.GET.get('page_size', 10))
+        query_body = {
+            "query": {
+                "match": {
+                    "display_name": author_name
+                }
+            },
+            "from": (page_num - 1) * page_size,
+            "size": page_size
+        }
+        es_res = elasticsearch_connection.search(index='author', body=query_body)
+        res = {
+            'total': es_res['hits']['total']['value'],
+            'authors': []
+        }
+        for hit in es_res['hits']['hits']:
+            res['authors'].append(hit['_source'])
+
+        return JsonResponse({
+            'code': SUCCESS,
+            'error': False,
+            'message': 'success',
+            'data': res
+        })
+    else:
+        return JsonResponse({
+            'code': METHOD_ERROR,
+            'error': True,
+            'message': '请求方式错误'
+        })
+
+
+# 根据作者id获取作者信息
+def get_author_by_id(request):
+    author_id = request.GET.get('author_id')
+    query_body = {
+        "query": {
+            "term": {
+                "id": author_id
+            }
+        }
+    }
+    es_res = elasticsearch_connection.search(index='author', body=query_body)
+    res = es_res['hits']['hits'][0]['_source']
     return JsonResponse({
-        'count': len(results),
-        'results': results
+        'code': SUCCESS,
+        'error': False,
+        'message': 'success',
+        'data': res
     })
 
 
-# 列出作者的所有作品
+# 根据作者id列出指定作者的所有作品，同样需要分页
 def get_works(request):
-    # 從work index裡面找
-    # 使用id還是display_name?
-    author_name = request.GET.get('author_name')
-
+    author_id = request.GET.get('author_id')
     query_body = {
         "query": {
             'nested': {
@@ -84,8 +75,8 @@ def get_works(request):
                     'nested': {
                         'path': 'authorships.author',
                         'query': {
-                            'match': {
-                                'authorships.author.display_name': author_name
+                            'term': {
+                                'authorships.author.id': author_id
                             }
                         }
                     }
@@ -96,5 +87,52 @@ def get_works(request):
     res = elasticsearch_connection.search(index='work', body=query_body)
 
     return JsonResponse({
-        'result': res
+        'code': 0,
+        'error': False,
+        'message': 'success',
+        'data': res
+    })
+
+
+# 热门作者
+def get_hot_authors(request):
+    # 从mysql里面搜索，按views排序
+    authors = Author.objects.order_by('-views')[:10]
+    res = []
+    for author in authors:
+        res.append({
+            'id': author.id,
+            'display_name': author.display_name,
+            'views': author.views
+        })
+
+    return JsonResponse({
+        'code': 0,
+        'msg': 'success',
+        'data': res
+    })
+
+
+# 根据指定id获取关联作者
+def get_related_authors(request):
+    pass
+
+
+# 根据作者id获取作者的维度信息
+def get_scholar_metrics(request):
+    author_id = request.GET.get('author_id')
+    query_body = {
+        "query": {
+            "term": {
+                "id": author_id
+            }
+        },
+        "_source": ["works_count", "cited_by_count", "summary_stats"]
+    }
+    es_res = elasticsearch_connection.search(index='author', body=query_body)
+    res = es_res['hits']['hits'][0]['_source']
+    return JsonResponse({
+        'code': SUCCESS,
+        'msg': 'success',
+        'data': res
     })
--- institution/views.py
+++ institution/views.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from elasticsearch_dsl import Search, connections, Q
 
 from NoBC.status_code import *
@@ -11,6 +13,50 @@ ES_NAME = 'institution'
 ES_CONN = connections.create_connection(alias=ES_NAME, hosts=[ELAS_HOST], timeout=20)
 
 
+def pagination(search, request) -> Search:
+    """
+    分页查询,将基础查询语句进行分页处理,返回具有分页功能的查询语句
+    :param request: 请求对象
+    :param search: Search对象
+    :return: Search对象
+    """
+    size = int(request.GET.get('size', 10))
+    last_sort = request.GET.getlist('last_sort', None)
+    page = int(request.GET.get('page', 1))
+    _from = (page - 1) * size
+    # 不需要after,使用(from, size)查询
+    if last_sort is None:
+        _from = (page - 1) * size
+        ret = search[_from: size]
+    # 使用search_after查询
+    else:
+        after_body = {
+            'search_after': last_sort,
+            'size': size
+        }
+        ret = search.update_from_dict(after_body)
+    return ret
+
+
+def get_return_data(search: Search, name: str) -> dict:
+    """
+    获取返回数据
+    :param name: 返回数据的名称
+    :param search: Search对象
+    :return: 返回数据data
+    """
+    ret = search.execute().to_dict()['hits']['hits']
+    data = []
+    for ele in ret:
+        dic = ele['_source']
+        dic['sort'] = ele['sort']
+        data.append(dic)
+    return_data = dict()
+    return_data['total'] = search.count()
+    return_data[name] = data
+    return return_data
+
+
 def getInstitutionList(request):
     """
     获取机构列表
@@ -21,25 +67,12 @@ def getInstitutionList(request):
     if request.method == 'GET':
         # 获取参数,使用search_after分页,需要获取:
         # 每页大小size,上一页最后一个数据的sort字段(若为"",默认为第一页)
-        size = int(request.GET.get('size', 10))
-        last_sort = request.GET.getlist('last_sort', None)
-        data = []
+        institutions = []
         # 默认id升序
         search = Search(using=ES_CONN, index='institution').query(Q('match_all')).sort('id')
         # 不需要after,使用(from, size)查询
-        if last_sort is None:
-            ret = search[0: size]
-        # 使用search_after查询
-        else:
-            after_body = {
-                'search_after': last_sort,
-                'size': size
-            }
-            ret = search.update_from_dict(after_body)
-        for ele in ret.execute().to_dict()['hits']['hits']:
-            dic = ele['_source']
-            dic['sort'] = ele['sort']
-            data.append(dic)
+        pagination_search = pagination(search, request)
+        data = get_return_data(pagination_search, 'institutions')
         return response(SUCCESS, '查询成功', data)
     else:
         return response(METHOD_ERROR, '请求方式错误', error=True)
@@ -78,25 +111,10 @@ def getInstitutionByKeyword(request):
         # 获取参数,使用search_after分页,需要获取:
         # 每页大小size,上一页最后一个数据的sort字段(若为"",默认为第一页)
         keyword = request.GET.get('keyword', "")
-        size = int(request.GET.get('size', 10))
-        last_sort = request.GET.getlist('last_sort', None)
-        data = []
         # 默认id升序
         search = Search(using=ES_CONN, index='institution').query('match', display_name=keyword).sort('id')
-        # 不需要after,使用(from, size)查询
-        if last_sort is None:
-            ret = search[0: size]
-        # 使用search_after查询
-        else:
-            after_body = {
-                'search_after': last_sort,
-                'size': size
-            }
-            ret = search.update_from_dict(after_body)
-        for ele in ret.execute().to_dict()['hits']['hits']:
-            dic = ele['_source']
-            dic['sort'] = ele['sort']
-            data.append(dic)
+        pagination_search = pagination(search, request)
+        data = get_return_data(pagination_search, 'institutions')
         return response(SUCCESS, '查询成功', data)
     else:
         return response(METHOD_ERROR, '请求方式错误', error=True)
