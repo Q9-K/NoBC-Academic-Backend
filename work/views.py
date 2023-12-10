88ad82330e8455e6bb7d94397d7565bff38131b1
Q9-K <20374319@buaa.edu.cn>
2023-12-10 20:49:27 +0800

    部署redis测试

--- work/views.py
+++ work/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from pprint import pprint
 # Create your views here.
 from django.http import JsonResponse
 from elasticsearch_dsl import connections, Search
@@ -13,13 +12,12 @@ INDEX_NAME = 'work'
 
 @allowed_methods(['GET'])
 def search(request):
-    request = request.GET
-    query_content = request.get('query_content')
-    time_from = request.get('time_from')
-    time_to = request.get('time_to')
-    journal = request.get('journal')
-    subject = request.get('subject')
-    institution = request.get('institution')
+    query_content = request.GET.get('query_content')
+    time_from = request.GET.get('time_from')
+    time_to = request.GET.get('time_to')
+    journal = request.GET.get('journal')
+    subject = request.GET.get('subject')
+    institution = request.GET.get('institution')
 
     if not query_content:
         return JsonResponse({
@@ -28,7 +26,7 @@ def search(request):
             'message': 'please input search text.',
             'data': {}
         })
-    search = Search(using=elasticsearch_connection, index=INDEX_NAME)
+    search = Search(using=elasticsearch_connection, index=INDEX_NAME).highlight('title', fragment_size=50)
     # if common search
     match = MultiMatch(query=query_content, fields=['abstract', 'title'])
     search = search.query(match)
@@ -48,6 +46,7 @@ def search(request):
     search = search.source(['title', 'abstract', 'authorships', 'best_oa_location'])
     search_results = search.execute()
     results = []
+    pprint(search_results['hits']['hits'])
     for hit in search_results:
         results.append(hit.to_dict())
     return JsonResponse({
@@ -63,10 +62,12 @@ def search(request):
 
 @allowed_methods(['GET'])
 def get_work(request):
+    ip = get_client_ip(request)
     request = request.GET
     id = request.get('id')
     search = Search(using=elasticsearch_connection, index=INDEX_NAME)
     search = search.filter('term', id=id)
+    result = search.execute()
     return JsonResponse({
         'code': SUCCESS,
         'error': False,
@@ -82,3 +83,12 @@ def get_work(request):
 @allowed_methods(['GET'])
 def get_popular_work(request):
     search = Search(using=elasticsearch_connection)
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
