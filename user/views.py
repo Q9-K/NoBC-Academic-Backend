0102949ab54c67e62dc54e949a98f22fd626f818
zh <1812643720@qq.com>
2023-12-13 10:17:49 +0800

    update user view with email

--- user/views.py
+++ user/views.py
@@ -1,14 +1,14 @@
 import random
+
+from django.core.mail import send_mail
+from NoBC.status_code import *
 from author.models import Author
 from config import BUAA_MAIL_USER
-from message.models import Message
 from utils.Md5 import create_md5, create_salt
 from utils.Response import response
 from utils.Token import generate_token
 from utils.Token import get_value
 from .models import User
-from NoBC.status_code import *
-from django.core.mail import send_mail
 
 
 def send_email(email) -> int:
@@ -50,13 +50,19 @@ def register_view(request):
             code = send_email(email)
             salt = create_salt()
             password_encode = create_md5(password, salt)
-            User.objects.create(name=name, password=password_encode, salt=salt, email=email)
+            # 创建用户, is_active=False, 未激活, 激活后才能登录,update_or_create,如果存在则更新,不存在则创建
+            User.objects.update_or_create(name=name, password=password_encode, salt=salt, email=email)
             return response(SUCCESS, '请注意查收邮件！', data=code)
         else:
             return response(PARAMS_ERROR, '提交字段名不可为空！', error=True)
 
 
 def active_user(request):
+    """
+    激活用户
+    :param request: correct_code(验证码), get_code(用户输入验证码), email
+    :return: [code, msg, data]
+    """
     if request.method != "POST":
         return response(METHOD_ERROR, '请求方法错误', error=True)
     else:
@@ -67,172 +73,189 @@ def active_user(request):
             try:
                 user = User.objects.get(email=email, is_active=False)
                 if get_code == correct_code:
-                    user.is_active = True
-                    user.save()
+                    user.activate()
                     return response(SUCCESS, '注册成功')
                 else:
                     return response(PARAMS_ERROR, '验证码错误', error=True)
-            except Exception as e:
+            except Exception:
                 return response(MYSQL_ERROR, '该用户已经注册过', error=True)
         else:
             return response(PARAMS_ERROR, '提交字段名不可为空！', error=True)
 
 
 def login_view(request):
+    """
+    登录
+    :param request: email, password
+    :return: [code, msg, data], 其中data为token(若登录成功,否则为None)
+    """
     if request.method != 'POST':
         return response(METHOD_ERROR, '请求方法错误', error=True)
     else:
-        name = request.POST.get('name')
+        email = request.POST.get('email', None)
         password = request.POST.get('password')
-        user = User.objects.get(name=name)
-        if user:
-            salt = user.salt
-            password_encode = create_md5(password, salt)
-            if password_encode != user.password:
-                return response(PARAMS_ERROR, '用户名或密码错误！', error=True)
-            else:
-                dic = {'name': name}
-                token = generate_token(dic, 60 * 60 * 24)
-                return response(SUCCESS, '登录成功！', data=token)
+        if email and password:
+            try:
+                user = User.objects.get(email=email, is_active=True)
+                salt = user.salt
+                password_encode = create_md5(password, salt)
+                if password_encode != user.password:
+                    return response(PARAMS_ERROR, '用户名或密码错误！', error=True)
+                else:
+                    dic = {'email': email, 'name': user.name}
+                    token = generate_token(dic, 60 * 60 * 24)
+                    return response(SUCCESS, '登录成功！', data=token)
+            except Exception:
+                return response(MYSQL_ERROR, '用户不存在！', error=True)
         else:
             return response(PARAMS_ERROR, '用户名或密码错误！', error=True)
 
 
-# 获取用户浏览记录
 def get_histories(request):
+    """
+    获取用户浏览记录
+    :param request: token
+    :return: [code, msg, data, error], 其中data为浏览记录列表
+    """
     if request.method != 'GET':
         return response(METHOD_ERROR, '请求方法错误', error=True)
     else:
-        token = request.GET.get('token')
+        token = request.GET.get('token', None)
         value = get_value(token)
         if value:
-            name = value['name']
+            email = value['email']
             try:
-                user = User.objects.get(name=name)
+                user = User.objects.get(email=email, is_active=True)
                 histories = user.histories.all()
-                data = []
-                for history in histories:
-                    data.append(history.to_string())
+                data = [history.to_string() for history in histories]
                 return response(SUCCESS, '获取用户浏览记录成功！', data=data)
-            except Exception as e:
+            except Exception:
                 return response(MYSQL_ERROR, '用户不存在！', error=True)
         else:
             return response(PARAMS_ERROR, 'token错误！', error=True)
 
 
-# 获取用户收藏记录
 def get_favorites(request):
+    """
+    获取用户收藏记录
+    :param request: token
+    :return: [code, msg, data, error], 其中data为收藏记录列表
+    """
     if request.method != 'GET':
         return response(METHOD_ERROR, '请求方法错误', error=True)
     else:
-        token = request.GET.get('token')
+        token = request.GET.get('token', None)
         value = get_value(token)
         if value:
-            name = value['name']
+            email = value['email']
             try:
-                user = User.objects.get(name=name)
+                user = User.objects.get(email=email, is_active=True)
                 favorites = user.favorites.all()
-                data = []
-                for favorite in favorites:
-                    data.append(favorite.to_string())
-                return response(SUCCESS, '获取用户收藏成功！', data=data)
-            except Exception as e:
+                data = [favorite.to_string() for favorite in favorites]
+                return response(SUCCESS, '获取用户收藏记录成功！', data=data)
+            except Exception:
                 return response(MYSQL_ERROR, '用户不存在！', error=True)
         else:
             return response(PARAMS_ERROR, 'token错误！', error=True)
 
 
-# 获取用户关注的领域
 def get_concept_focus(request):
+    """
+    获取用户关注的领域
+    :param request: token
+    :return: [code, msg, data, error], 其中data为领域列表
+    """
     if request.method != 'GET':
         return response(METHOD_ERROR, '请求方法错误', error=True)
     else:
         token = request.GET.get('token')
         value = get_value(token)
         if value:
-            name = value['name']
+            email = value['email']
             try:
-                user = User.objects.get(name=name)
+                user = User.objects.get(email=email, is_active=True)
                 concepts = user.concept_focus.all()
-                data = []
-                for concept in concepts:
-                    data.append(concept.to_string())
-                return response(SUCCESS, '获取用户关注领域成功！', data=data)
-            except Exception as e:
+                data = [concept.to_string() for concept in concepts]
+                return response(SUCCESS, '获取用户关注的领域成功！', data=data)
+            except Exception:
                 return response(MYSQL_ERROR, '用户不存在！', error=True)
         else:
             return response(PARAMS_ERROR, 'token错误！', error=True)
 
 
-# 获取用户站内信
 def get_messages(request):
+    """
+    获取用户站内信
+    :param request: token
+    :return: [code, msg, data, error], 其中data为站内信列表
+    """
     if request.method != 'GET':
         return response(METHOD_ERROR, '请求方法错误', error=True)
     else:
         token = request.GET.get('token')
         value = get_value(token)
         if value:
-            name = value['name']
+            email = value['email']
             try:
-                user = User.objects.get(name=name)
-                messages = Message.objects.filter(receiver=user)
-                data = []
-                for message in messages:
-                    data.append(message.to_string())
+                user = User.objects.get(email=email, is_active=True)
+                messages = user.msg.all()
+                data = [message.to_string() for message in messages]
                 return response(SUCCESS, '获取用户站内信成功！', data=data)
-            except Exception as e:
+            except Exception:
                 return response(MYSQL_ERROR, '用户不存在！', error=True)
         else:
             return response(PARAMS_ERROR, 'token错误！', error=True)
 
 
-# 获取关注的学者
 def get_scholar_focus(request):
+    """
+    获取关注的学者
+    :param request: token
+    :return: [code, msg, data, error], 其中data为学者列表
+    """
     if request.method != 'GET':
         return response(METHOD_ERROR, '请求方法错误', error=True)
     else:
         token = request.GET.get('token')
         value = get_value(token)
         if value:
-            name = value['name']
+            email = value['email']
             try:
-                user = User.objects.get(name=name)
-                scholar = user.scholar_identity
-                if scholar:
-                    data = scholar.to_string()
-                else:
-                    data = None
-                return response(SUCCESS, '获取关注的学者成功！', data=data)
-            except Exception as e:
+                user = User.objects.get(email=email, is_active=True)
+                scholars = user.follows.all()
+                data = [scholar.to_string() for scholar in scholars]
+                return response(SUCCESS, '获取用户关注的学者成功！', data=data)
+            except Exception:
                 return response(MYSQL_ERROR, '用户不存在！', error=True)
         else:
             return response(PARAMS_ERROR, 'token错误！', error=True)
 
 
-# 关注学者
 def follow_scholar(request):
+    """
+    关注学者
+    :param request: scholar_id, token
+    :return: [code, msg, data, error]
+    """
     if request.method != 'POST':
         return response(METHOD_ERROR, '请求方法错误', error=True)
     else:
         token = request.GET.get('token')
         value = get_value(token)
         if value:
-            name = value['name']
+            email = value['email']
             try:
-                user = User.objects.get(name=name)
+                user = User.objects.get(email=email, is_active=True)
                 scholar_id = request.POST.get('scholar_id', None)
-                if scholar_id:
-                    try:
-                        scholar = Author.objects.get(id=scholar_id)
-                    except Exception as e:
-                        # 不存在这个学者则创建
-                        scholar = Author.objects.create(id=scholar_id)
-                        scholar.save()
+                if not scholar_id:
+                    return response(PARAMS_ERROR, '缺少学者id！', error=True)
+                try:
+                    scholar = Author.objects.get(id=scholar_id)
                     user.follows.add(scholar)
-                    return response(SUCCESS, '关注学者成功')
-                else:
-                    return response(PARAMS_ERROR, '学者id不能为空', error=True)
-            except Exception as e:
+                    return response(SUCCESS, '关注学者成功！')
+                except Exception:
+                    return response(MYSQL_ERROR, '学者不存在！', error=True)
+            except Exception:
                 return response(MYSQL_ERROR, '用户不存在！', error=True)
         else:
             return response(PARAMS_ERROR, 'token错误！', error=True)
