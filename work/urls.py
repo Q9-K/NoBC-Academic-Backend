ebb53615ceed33128c8f4213940b8b4f194d6139
Q9-K <20374319@buaa.edu.cn>
2023-12-21 02:25:12 +0800

    调整一波Work的结构

--- work/urls.py
+++ work/urls.py
@@ -1,8 +1,11 @@
 from .views import *
 from django.urls import path
+
 urlpatterns = [
     path('search/', search),
-    path('get_popular_work/', get_popular_work),
+    path('advanced_search/', advanced_search),
+    path('get_popular_works/', get_popular_works),
     path('get_work/', get_work),
+    path('get_suggestion/', get_suggestion),
     path('get_reply/', get_reply)
 ]
