542d23b6c32e4526961754898e53f0e8e7f4c090
Q9-K <20374319@buaa.edu.cn>
2023-12-22 17:43:49 +0800

    调整一波Work的结构

--- Import/WorkImport.py
+++ Import/WorkImport.py
@@ -9,10 +9,7 @@ from elasticsearch.helpers import parallel_bulk
 from elasticsearch import Elasticsearch
 from path import data_path
 from collections import deque
-import logging
 
-logging.basicConfig(level=logging.INFO,  # 设置日志级别
-                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 connections.create_connection(hosts=['localhost'], timeout=60, http_auth=('elastic', 'buaaNOBC2121'))
 client = Elasticsearch(hosts=['localhost'], timeout=60, http_auth=('elastic', 'buaaNOBC2121'))
@@ -239,9 +236,8 @@ def process_files(folder):
     for file in files:
         if file not in imported_files:
             run(os.path.join(folder, file))
-            logging.info(file + 'import success!')
         else:
-            logging.info(file + 'has been imported!')
+            print(file + ' has been imported!')
 
 
 def save_imported_files(file_name):
