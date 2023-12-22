29c98638ca000237e22be25ce043a7b3c2680106
= <21371480@buaa.edu.cn>
2023-12-22 20:32:42 +0800

    优化chatPDF

--- work/views.py
+++ work/views.py
@@ -334,26 +334,28 @@ def get_reply(request):
     # file_path = input("input pdf path: ")
     msg = request.GET.get('msg', '')
     url = request.GET.get('pdf_url', '')
+    needReload = request.GET.get('needReload', '')
     os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
     os.environ["OPENAI_API_BASE"] = "https://api.132999.xyz/v1"
 
-    # 根据文件类型来定义一个loader，不同的loader能够解析不同的文件内容，最终都会解析为一个大文本
+    # 根据文件类型来定义一个loader
     destination_file = "./currentPDF.pdf"
-    download_webpage(url, destination_file)
+    if needReload:
+        download_webpage(url, destination_file)
     loader = PyPDFLoader(destination_file)
 
-    # 定义文本分块的规则，这里用了一个很简单的规则，按照默认的分隔符来切割文本，使得每一段不超过1000个字符
+    # 定义文本分块的规则
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     chunks = loader.load_and_split(splitter)
 
-    # 定义文本的embedding，也就是如何把文本转换为向量。默认使用sentence-transformers这个免费的模型，也可以使用OpenAI提供的收费接口
+    # 定义文本的embedding，也就是如何把文本转换为向量
     embeddings = OpenAIEmbeddings()
 
-    # 实现一个本地的文档语义搜索，在存入一堆chunk之后，能够随时检索和问题最相关的一些chunk。Chroma就是一个比较流行的vector store
+    # 实现一个本地的文档语义搜索，在存入一堆chunk之后，能够随时检索和问题最相关的一些chunk
     db = Chroma.from_documents(chunks, embeddings)
 
-    # 本地搜索到的chunk会作为context，和问题一起提交给LLM来处理。我们当然要使用ChatGPT模型了，比GPT-3.0又好又便宜
+    # 本地搜索到的chunk会作为context，
     llm = ChatOpenAI(temperature=0)
 
     # chain是LangChain里的概念，其实就相当于定义了一个流程，这里我们提供的参数就是文档语义搜索工具以及LLM
