8ed02710edf2c1c7df7a925d97324fcaab5ab382
zh <1812643720@qq.com>
2023-12-24 22:33:17 +0800

    fix message read

--- user/views.py
+++ user/views.py
@@ -561,6 +561,7 @@ def change_user_info(request):
     position = request.POST.get('position', '')
     organization = request.POST.get('organization', '')
     subject = request.POST.get('subject', '')
+    gender = request.POST.get('gender', '')
     # 进行更新
     user = request.user
     user: User
@@ -569,6 +570,7 @@ def change_user_info(request):
     user.position = position
     user.organization = organization
     user.subject = subject
+    user.gender = gender
     user.save()
     return response(SUCCESS, '修改用户信息成功！')
 
@@ -779,7 +781,6 @@ def check_author_authentication(request):
 @allowed_methods(['POST'])
 @login_required
 def read_message(request):
-    user = request.user
     message_id = request.POST.get('message_id', None)
     if message_id:
         # 将消息改为已读
